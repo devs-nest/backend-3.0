ae4c228c02f77995da099d49692ba6a592c6a345
Thahsi <61452944+thahseen53@users.noreply.github.com>
2022-12-15 23:03:45 +0530

    Delete Day 7 (tinyurl) directory

--- Day 7 (tinyurl)/config/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-//creating a database in sqlite
-const createDB = new Sequelize("test-db","user","pass",{
-  dialect:"sqlite",
-  host:"./config/db.sqlite",
-});
-
-module.exports = createDB;
--- Day 7 (tinyurl)/routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-
-router.get('/',async(req,res)=>{
-  const htmlPath =path.join(__dirname,'public','index.html');
-  res.sendFile(htmlPath);
-})
-module.exports = router;
